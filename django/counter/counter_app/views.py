b51dddd9329c8e02cfb2004c3431b917e36a8816
Hani-hash <hani.ikhmais@gmail.com>
2021-05-20 15:04:06 +0300

    adding the user assignment from ORM Django

--- django/counter/counter_app/views.py
+++ django/counter/counter_app/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render, HttpResponse, redirect
 def index(request):
     if not('number' in request.session ):
-        request.session["number"]=0
+        request.session["number"]=1
     if not('num' in request.session ):
-        request.session["num"]=0
+        request.session["num"]=1
     request.session["number"]+=1
     request.session["num"]+=1
     return render(request,"index.html")
 
 def destroy(request):
     request.session["number"]=0
-    return redirect("/")
+    
+    return render(request,"index.html")
 
 def addtwo(request):
     request.session["number"]+=1
-    return redirect("/")
+    return render(request,"index.html")
 
 def increment(request,):
-
     request.session["number"]+=int(request.POST["number1"])-1
-    return redirect("/")
+    return render(request,"index.html")
